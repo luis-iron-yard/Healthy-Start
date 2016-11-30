69966244f9a497e94579d72eb720e604526a1605
keith brooks <kbrook10@gmail.com>
2016-11-30 16:35:44 -0500

    Update signin page to confirm backend approval

--- Healthy_Start/webpack/application.js
+++ Healthy_Start/webpack/application.js
@@ -6,7 +6,6 @@ import Signup from './components/Signup'
 // import Search from './components/Search'
 // import Recipe_Id from './components/Recipe_Id'
 // import Favorites from './components/Favorites'
-//<Route path="/signup" component={Signup} />
 //<Route path="/search" component={Search} />
 //<Route path="/recipe_id/:id" component={Recipe_Id} />
 //<Route path="/favorites" component={Favorites} />
@@ -16,6 +15,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
         <Router history={browserHistory}>
             <Route path='/users/sign_in' component={Landing} />
             <Route path='/users/sign_up' component={Signup} />
+            {/* <Route path='/users/' component={Interest} />
+            <Route path='/users/' component={Favorites} /> */}
         </Router>
         ,document.getElementById('app')
     )
--- Healthy_Start/webpack/components/landing.js
+++ Healthy_Start/webpack/components/landing.js
@@ -5,6 +5,38 @@ import Signup from './Signup'
 class Landing extends React.Component {
     constructor(props) {
         super(props)
+        this.confirmUser = this.confirmUser.bind(this)
+        this.state = {
+            login: '',
+            password: '',
+        }
+    }
+    confirmUser() {
+        console.log('The Ajax is compiling data to fire off...')
+        var formData = {
+            login: this.state.login,
+            password: this.state.password,
+        }
+        console.log(formData)
+        fetch("/users", {
+            body:JSON.stringify(
+                {user_login: formData}
+            ),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            // if () {
+            //
+            // }
+            // else {
+            //     window.location.href = '/users/sign_in'
+            // }
+            console.log(response)
+        })
     }
     render () {
         return (
@@ -20,23 +52,23 @@ class Landing extends React.Component {
                             <p>Need an account? <Link to="/users/sign_up">SignUp</Link></p>
                             <div className="form-group">
                                 <label htmlFor="emailInput">Email address</label>
-                                <input type="email" className="form-control" id="signInEmail" aria-describedby="emailAssistance" placeholder="Please Enter email" />
+                                <input type="email" className="form-control" id="signInEmail" aria-describedby="emailAssistance" placeholder="Please Enter email" onChange={(e)=>this.setState({login: e.target.value})} required />
                                 <small id="emailAssistance" className="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="passwordInput">Password</label>
-                                <input type="password" className="form-control" id="passwordInput" placeholder="Password" />
+                                <input type="password" className="form-control" id="passwordInput" placeholder="Password" onChange={(e)=>this.setState({password: e.target.value})} required />
                             </div>
-                            <div className="form-group">
+                            {/* <div className="form-group">
                                 <label htmlFor="selectInterest">Select Interest</label>
                                 <select className="form-control" id="selectInterest">
                                     <option>Nutritional Topics</option>
                                     <option>Common Complaint Topics</option>
                                 </select>
-                            </div>
+                            </div> */}
                             <div className="row">
                                 <div className="col-sm-6">
-                                    <button className="btn btn-primary">Login</button>
+                                    <button className="btn btn-primary" onClick={this.confirmUser}>Login</button>
                                 </div>
                                 <div className="col-sm-6">
                                     <button className="btn btn-default">Guest</button>
