84f36716be9198d96092c64cd639ebf133f67d8d
keith brooks <kbrook10@gmail.com>
2016-12-06 18:13:54 -0500

    Update new home app frame to display nutrition illness search and profile views

--- Healthy_Start/webpack/components/Home.js
+++ Healthy_Start/webpack/components/Home.js
@@ -1,12 +1,39 @@
 import React from 'react'
-
+import { browserHistory } from 'react-router'
 
 class Home extends React.Component {
     render() {
+        var headerStyle = {
+            fontFamily: 'Lobster',
+            fontSize: 25,
+            backgroundColor: '#000',
+            color: '#fff',
+            padding: 25,
+            textAlign: 'left',
+        }
+        var bodyStyle = {
+            padding: 20,
+            margin: 0,
+            backgroundColor: '#66ccff',
+            height: 700,
+        }
         return(
-            <div>
-                <h1>Home</h1>
-                {this.props.children}
+            // <div>
+            //     <h1>Home</h1>
+            //     {this.props.children}
+            // </div>
+            <div style={bodyStyle}>
+                {/* <h1>Home</h1> */}
+                <ul style={headerStyle}>
+                    <li onClick={() => browserHistory.push('home')}>Healthy Start</li>
+                    <li onClick={() => browserHistory.push('/home/nutrition')}>Nutrition</li>
+                    <li onClick={() => browserHistory.push('/home/illness')}>Illness</li>
+                    <li onClick={() => browserHistory.push('/home/search')}>Search</li>
+                    <li onClick={() => browserHistory.push('/home/profile')}>Profile</li>
+                </ul>
+                <div>
+                    {this.props.children}
+                </div>
             </div>
         )
     }
