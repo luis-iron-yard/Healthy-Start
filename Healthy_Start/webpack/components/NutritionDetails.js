28791c0bbff803db06e5a3c7d525ccc029dca099
keith brooks <kbrook10@gmail.com>
2016-12-07 17:46:26 -0500

    Update to mock up nutrient description w/ benefits

--- Healthy_Start/webpack/components/NutritionDetails.js
+++ Healthy_Start/webpack/components/NutritionDetails.js
@@ -8,8 +8,13 @@ class NutritionDetails extends React.Component {
     render() {
         return(
             <div>
-                <h1>Nutrition Details</h1>
-
+                <h4 className='ns--sectionHeader'>Nutrition Details</h4>
+                <div className='ns--card'>
+                    <h5>Nutrient Name: </h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p><br />
+                    <h5>Nutrien Benefits:</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+                </div>
             </div>
         )
     }
