5c5e81cfd58607cb43606bcd33afd2afc31355d2
keith brooks <kbrook10@gmail.com>
2016-12-07 15:56:07 -0500

    Update styling for all home reference pages

--- Healthy_Start/webpack/components/Nutrition.js
+++ Healthy_Start/webpack/components/Nutrition.js
@@ -9,12 +9,22 @@ class Nutrition extends React.Component {
     }
     render() {
         return(
-            <div>
-                <h4>Nutrition</h4>
+            <div className='viewSection'>
+                <h4 className='pageTitle'>Nutrition</h4>
                 <div className='container'>
                     <div className='row'>
                         <div className='col-sm-4'>
                             {/* <NutritionList /> */}
+                            <p>Nutrition List</p>
+                            <ul className='ns--nutritionlist'>
+                                <li className='ns-listItem'>Home</li>
+                                <li className='ns-listItem'>Home1</li>
+                                <li className='ns-listItem'>Home2</li>
+                                <li className='ns-listItem'>Home3</li>
+                                <li className='ns-listItem'>Home4</li>
+                                <li className='ns-listItem'>Home5</li>
+                                <li className='ns-listItem'>Home6</li>
+                            </ul>
                         </div>
                         <div className='col-sm-8'>
                             <NutritionDetails  /><hr />
