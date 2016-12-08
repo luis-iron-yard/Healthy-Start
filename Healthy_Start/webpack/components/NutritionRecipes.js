38cf8eb21c29e010fbd0e5197593aefde0caafdc
keith brooks <kbrook10@gmail.com>
2016-12-07 19:28:57 -0500

    Update nutrition page to display recipes, but hardcoded the description details and food items...Need further help

--- Healthy_Start/webpack/components/NutritionRecipes.js
+++ Healthy_Start/webpack/components/NutritionRecipes.js
@@ -12,9 +12,27 @@ class NutritionRecipes extends React.Component {
         var cardStyle = {
             border: '2px solid black',
         }
+        var recipes = this.props.recipes.map((recipe, i) =>{
+            return <li className='ns-listItemRecipe' key={i}>
+                <div className="card">
+                    <div className="card-block">
+                        <h4 className="card-title">Nutrition</h4>
+                        <h6 className="card-subtitle text-muted">{recipe.recipe_name}</h6>
+                    </div>
+                    <img style={imgStyle} src={recipe.food_image} alt="Card image"/>
+                    <div className="card-block">
+                        <a href={recipe.instructions} className="card-link">Instructions</a>&nbsp;&nbsp;&nbsp;
+                        <a href="#" className="card-link">Save to Favorites</a>
+                    </div>
+                </div>
+            </li>
+        })
         return(
             <div>
                 <h1>Nutrition Recipes</h1>
+                <ol>
+                    {recipes}
+                </ol>
                 <div className="card">
                   <div className="card-block">
                     <h4 className="card-title">Nutrition</h4>
@@ -22,9 +40,8 @@ class NutritionRecipes extends React.Component {
                   </div>
                   <img style={imgStyle} src="https://unsplash.it/800/?random" alt="Card image"/>
                   <div className="card-block">
-                    <p className="card-text">Foods: Broccoli, Sweet Potatoes, ...</p>
-                    <a href="#" className="card-link">Card link</a>&nbsp;&nbsp;&nbsp;
-                    <a href="#" className="card-link">Another link</a>
+                    <a href="#" className="card-link">Instructions</a>&nbsp;&nbsp;&nbsp;
+                    <a href="#" className="card-link">Save to Favorites</a>
                   </div>
                 </div>
             </div>
