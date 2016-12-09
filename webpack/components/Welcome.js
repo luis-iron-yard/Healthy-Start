954ba89a82800f2daa092fc74c2c6ee95f2f8984
keith brooks <kbrook10@gmail.com>
2016-12-08 19:32:31 -0500

    Update login functionality to direct to nutritions page and save authentication token

--- webpack/components/Welcome.js
+++ webpack/components/Welcome.js
@@ -4,28 +4,87 @@ import Signup from './Signup'
 import Home from './Home'
 
 class Welcome extends React.Component {
+    constructor(props) {
+        super(props)
+        this.collectUserInput = this.collectUserInput.bind(this)
+        this.authenticateUser = this.authenticateUser.bind(this)
+        this.state = {
+            login: '',
+            password: '',
+        }
+    }
+
+    collectUserInput() {
+        //Collecting values for user input fields...
+        //Compile values into form variable to send out for authentication...
+        var formData = {
+            login: this.state.login,
+            password: this.state.password,
+        }
+        this.authenticateUser(formData)
+    }
+
+    authenticateUser(formData) {
+        console.log('The Ajax is about to send off user data...')
+        //Send user information to server for authentication...
+        fetch("/api/users/sign_in", {
+            body:JSON.stringify(
+                {user_login: formData}
+            ),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            sessionStorage.setItem('authentication_token', response.authentication_token)
+            console.log('About to redirect to nutritions')
+            console.log(response.authentication_token)
+            //If user info. is valid redirect to home page...
+            window.location.href = '/home/nutrition'
+            console.log('Got all the way here...')
+        })
+    }
+
     render() {
         var welcomeStyling = {
             margin: 'auto',
-            padding: '175px',
+            padding: '150px',
             fontSize: '1.5em',
             textAlign: 'center',
         }
         var titleStyling = {
             fontSize: '2.5em',
             fontFamily: 'Lobster, Helvetica, sans-serif',
+            color: '#fff',
         }
         var buttonWelcomeStyling = {
             padding: 20,
             margin: 30,
             borderRadius: 15,
+            color: '#fff',
+            border: '4px solid #66ccff',
+            boxShadow: '2px 2px 2px #fff',
+        }
+        var titleEmphasis = {
+            color: '#000',
         }
         return(
             <div style={welcomeStyling}>
-                <h1 style={titleStyling}>Welcome Hello World!</h1>
+                <h1 style={titleStyling}><span style={titleEmphasis}>Welcome to</span> Healthy Start!</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <div>
-                    <button style={buttonWelcomeStyling} onClick='' className='ws--Button'>Login</button>
+                <div><br />
+                    <div className="form-group">
+                        <label htmlFor="emailInput">Email address</label>
+                        <input type="email" className="form-control" id="signInEmail" aria-describedby="emailAssistance" placeholder="Please Enter email" onChange={(e)=>this.setState({login: e.target.value})} required />
+                        <small id="emailAssistance" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="passwordInput">Password</label>
+                        <input type="password" className="form-control" id="passwordInput" placeholder="Password" onChange={(e)=>this.setState({password: e.target.value})} required />
+                    </div>
+                    <button style={buttonWelcomeStyling} onClick={this.collectUserInput} className='ws--Button'>Login</button>
                     <Link to="/api/users/sign_up"><button style={buttonWelcomeStyling} onClick='' className='ws--Button'>Sign Up</button></Link>
                 </div>
 
