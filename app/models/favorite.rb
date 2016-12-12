ed1ac4228c7d317be7abfbebae51171c0fde6f53
llancon <rdzalb@gmail.com>
2016-12-12 11:28:21 -0500

    Added favorites mailer and also added favorites index for so users can see all their favorited recipes

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,7 +1,12 @@
 class Favorite < ApplicationRecord
+  after_create :send_favorite
   belongs_to :recipe
   belongs_to :user
   validates_uniqueness_of :user_id, scope: :recipe_id
 
+  private
 
+  def send_favorite
+    UserFavoritesMailer.saved_recipe(self).deliver
+  end
 end
